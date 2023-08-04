cda157b74292076308c19c03973a376ab938641e
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-08-04 21:55:43 +0400

    added task 19 - Decode A Web Page Two

---
+++ decode_a_web_page_two.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+    request = get_request(url)
+    p_tags = get_p_tags(request)
+    article = generate_article(p_tags)
+    print("".join(article[4:]))
+
+def get_request(url):
+    return requests.get(url)
+
+def get_p_tags(request):
+    request_html = request.text
+    soup = BeautifulSoup(request_html,"html.parser")
+    a_tags = soup.find_all('p')
+    return a_tags
+
+def generate_article(p_tags):
+    return [i.get_text() for i in p_tags]
+
+if __name__ == "__main__":
+    main()
