ae45c15f002a5474fa0f71763d7fd77ad2c65a3e
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-08-15 13:42:15 +0400

    updated task 22 - Read From File - seperated file opening function with counter

--- read_from_file.py
+++ read_from_file.py
@@ -1,17 +1,20 @@
 def main():
     file_name = 'text_files/nameslist.txt'
-    print(open_file(file_name))
+    listed_elements = file_to_list(file_name)
+    print(dict_counter(listed_elements))
 
-def open_file(filename):
+def file_to_list(filename):
     with open(filename, 'r') as file:
         unpacked_list = file.read().split()
-        
-        mydict = {}
-        for i in unpacked_list:
-            if i in mydict.keys():
-                mydict[i] += 1
-            else:
-                mydict[i] = 1
+    return unpacked_list
+
+def dict_counter(listed_elements):
+    mydict = {}
+    for i in listed_elements:
+        if i in mydict.keys():
+            mydict[i] += 1
+        else:
+            mydict[i] = 1
     return mydict
 
 if __name__ == "__main__":
