a7f5b17c55395cf8295c5f3a33018ecf86904a79
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-09-06 15:40:35 +0400

    updated task 9 - changed the wrongly implemented logic

--- guessing_game_one.py
+++ guessing_game_one.py
@@ -1,33 +1,31 @@
 import random
 
 def main():
-    count = 0 
+    count = 0
+    random_number = random.randint(1,9)
     while True:
-        random_number = random.randint(1,9)
         ask = get_input()
         if ask is None:
             break
         if compare_input(ask, random_number):
-            count += 1
             print(f"You are CORRECT! The number was {random_number}")
+            break
         else:
+            count += 1
             print("Too high!" if ask > random_number else "Too low!")
-    print("Your score:",count)
+    print(f"It took you {count} guesses!")
 
 def get_input():
     while True:
-        try:
-            ask = input("Number between 1 and 9 or 'exit' to exit: ")
-            if ask.lower() == "exit":
-                return None
-            
-            ask = int(ask)
-            if ask > 9 or ask < 1:
-                print("Invalid input, your number must be between 1 and 9.")
-            else:
-                return ask
-        except ValueError:
-            print("Invalid input! Try again.")
+        ask = input("Number between 1 and 9 or 'exit' to exit: ")
+        if ask.lower() == "exit":
+            return None
+        
+        ask = int(ask)
+        if ask > 9 or ask < 1:
+            print("Invalid input, your number must be between 1 and 9.")
+        else:
+            return ask
 
 def compare_input(ask, random_number):
     return ask == random_number
