a2994a3e8ca694c3b618401c30ec53d6b8326d84
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-09-06 15:15:02 +0400

    added task 38 - f Strings

---
+++ f_strings.py
@@ -0,0 +1,24 @@
+def main():
+    name = input("Please enter you name: ").strip().capitalize()
+    age = get_age()
+    
+    future_year = year_calculator(age)
+    print(f"Hello {name},\nYou will be 100 years old in {future_year}")
+
+def get_age():
+    while True:
+        try:
+            age = int(input("Please enter you age: "))
+            if 0<= age <= 100:
+                return age
+            else:
+                print("Invalid Age!")
+        except ValueError:
+            print("Invalid input!")
+            
+def year_calculator(age):
+    future_year = 100-age+2023
+    return future_year
+
+if __name__ == "__main__":
+    main()
